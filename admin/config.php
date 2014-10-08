522727a94653c1c9198aa4fb4116424e1e39cf11
fortislogo <fortislogo@hotmail.com>
2014-10-08 08:22:36 +0800

    config

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://bling.local/admin/');
+define('HTTP_CATALOG', 'http://bling.local/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://bling.local/admin/');
+define('HTTPS_CATALOG', 'http://bling.local/');
+
+// DIR
+define('DIR_APPLICATION', 'D:\projects\bling/admin/');
+define('DIR_SYSTEM', 'D:\projects\bling/system/');
+define('DIR_DATABASE', 'D:\projects\bling/system/database/');
+define('DIR_LANGUAGE', 'D:\projects\bling/admin/language/');
+define('DIR_TEMPLATE', 'D:\projects\bling/admin/view/template/');
+define('DIR_CONFIG', 'D:\projects\bling/system/config/');
+define('DIR_IMAGE', 'D:\projects\bling/image/');
+define('DIR_CACHE', 'D:\projects\bling/system/cache/');
+define('DIR_DOWNLOAD', 'D:\projects\bling/download/');
+define('DIR_LOGS', 'D:\projects\bling/system/logs/');
+define('DIR_CATALOG', 'D:\projects\bling/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'bling');
+define('DB_PREFIX', 'oc_');
+?>
---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://bling.local/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://bling.local/');
+
+// DIR
+define('DIR_APPLICATION', 'D:\projects\bling/catalog/');
+define('DIR_SYSTEM', 'D:\projects\bling/system/');
+define('DIR_DATABASE', 'D:\projects\bling/system/database/');
+define('DIR_LANGUAGE', 'D:\projects\bling/catalog/language/');
+define('DIR_TEMPLATE', 'D:\projects\bling/catalog/view/theme/');
+define('DIR_CONFIG', 'D:\projects\bling/system/config/');
+define('DIR_IMAGE', 'D:\projects\bling/image/');
+define('DIR_CACHE', 'D:\projects\bling/system/cache/');
+define('DIR_DOWNLOAD', 'D:\projects\bling/download/');
+define('DIR_LOGS', 'D:\projects\bling/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'bling');
+define('DB_PREFIX', 'oc_');
+?>
